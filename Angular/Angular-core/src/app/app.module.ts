164b0d20f50aeaa93e54824f1155961b57de4e4b
dharmendra4522 <dharmendravishwakarma0711@gmail.com>
2026-06-09 11:20:18 +0530

    Save work before Angular 16 upgrade

--- Angular/Angular-core/src/app/app.module.ts
+++ Angular/Angular-core/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {UserAuthModule} from './user-auth/user-auth.module';
-import { DataBindingComponent } from './data-binding/data-binding.component'
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectivesComponent } from './directives/directives.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    DirectivesComponent,
     // DataBindingComponent
   ],
   imports: [
---
+++ Angular/Angular-core/src/app/directives/directives.component.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent {
+  isLogin = signal(false);
+}
